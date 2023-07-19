960851bdaa72500c16e13e375c871a2409793bc1
christianc102 <christianchiu@college.harvard.edu>
2023-07-19 13:03:30 -0400

    cleaned up visualizations and scrapped arima for simple moving averages

--- prelim_viz.py
+++ prelim_viz.py
@@ -9,6 +9,12 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+
+sns.set_theme()
+sns.color_palette("deep")
+
+units_dict = {"NO2": "ppb", "O3": "ppb", "PM": u"\u03bcg m$^{-3}$"}
 
 if len(sys.argv) != 3:
     print("Urban area or pollutant specified improperly--using defaults...")
@@ -27,39 +33,73 @@ times = dataset['time'].dt.strftime("%m/%d/%Y")
 mean_data_t_series = dataset.mean(dim=["x", "y"])
 fig = plt.figure()
 mean_data_t_series_df = mean_data_t_series.to_dataframe()[[f"{pollutant} concentration"]].droplevel(1)
-sns.lineplot(data=mean_data_t_series_df)
-plt.xticks(rotation=90)
+mean_data_t_series_df = mean_data_t_series_df.rename(columns={f"{pollutant} concentration": f"{pollutant} concentration ({units_dict[pollutant]})"})
+print(mean_data_t_series_df.info())
+window_length = [50, 200, 730]
+mean_data_t_series_df[f'{window_length[0]}-day moving average'] = mean_data_t_series_df[f"{pollutant} concentration ({units_dict[pollutant]})"].rolling(window=window_length[0]).mean()
+mean_data_t_series_df[f'{window_length[1]}-day moving average'] = mean_data_t_series_df[f"{pollutant} concentration ({units_dict[pollutant]})"].rolling(window=window_length[1]).mean()
+mean_data_t_series_df[f'{window_length[2]//365}-yr moving average'] = mean_data_t_series_df[f"{pollutant} concentration ({units_dict[pollutant]})"].rolling(window=window_length[2]).mean()
+print(mean_data_t_series_df.info())
+sns.lineplot(x=mean_data_t_series_df.index, y=f"{pollutant} concentration ({units_dict[pollutant]})", data=mean_data_t_series_df, alpha=0.5, linewidth=0.5, label="Data")
+sns.lineplot(x=mean_data_t_series_df.index, y=f"{window_length[0]}-day moving average", data=mean_data_t_series_df, label=f"{window_length[0]}-day moving average")
+sns.lineplot(x=mean_data_t_series_df.index, y=f"{window_length[1]}-day moving average", data=mean_data_t_series_df, label=f"{window_length[1]}-day moving average")
+sns.lineplot(x=mean_data_t_series_df.index, y=f"{window_length[2]//365}-yr moving average", data=mean_data_t_series_df, label=f"{window_length[2]//365}-year moving average")
+plt.xticks(fontsize=12)
 plt.savefig(f"./Visualizations/{urban_area}{pollutant}mean_time_series.png")
 
-# find order of d in ARIMA
-result = adfuller(mean_data_t_series_df.to_numpy())
-print(f'ADF Statistic: {result[0]}')
-print(f'p-value: {result[1]}')
+# # find order of d in ARIMA
+# result = adfuller(mean_data_t_series_df[f"{pollutant} concentration"].to_numpy())
+# print(f'ADF Statistic: {result[0]}')
+# print(f'p-value: {result[1]}')
 
-# find order of p in ARIMA
-fig, axes = plt.subplots(1, 2, sharex=True)
-axes[0].plot(np.ediff1d(mean_data_t_series_df.to_numpy()))
-axes[0].set_title('1st Differencing')
-axes[1].set(ylim=(0,5), xlim=(0,40))
-plot_pacf(np.ediff1d(mean_data_t_series_df.to_numpy()), ax=axes[1])
-plt.savefig("./Tests/PACF.png")
+# # find order of p in ARIMA
+# fig, axes = plt.subplots(1, 2, sharex=True)
+# axes[0].plot(np.ediff1d(mean_data_t_series_df[f"{pollutant} concentration"].to_numpy()))
+# axes[0].set_title('1st Differencing')
+# axes[1].set(ylim=(0,5), xlim=(0,40))
+# plot_pacf(np.ediff1d(mean_data_t_series_df[f"{pollutant} concentration"].to_numpy()), ax=axes[1])
+# plt.savefig("./Tests/PACF.png")
 
-# bounds_dict = {"NO2": 53, "O3": 70, "PM": 35}
-# units_dict = {"NO2": "ppb", "O3": "ppb", "PM": u"\u03bcg m$^{-3}$"}
+# # find order of q in ARIMA
+# fig, axes = plt.subplots(1, 2, sharex=True)
+# axes[0].plot(np.ediff1d(mean_data_t_series_df[f"{pollutant} concentration"].to_numpy()))
+# axes[0].set_title('1st Differencing')
+# axes[1].set(ylim=(0,5), xlim=(0,40))
+# plot_acf(np.ediff1d(mean_data_t_series_df[f"{pollutant} concentration"].to_numpy()), ax=axes[1])
+# plt.savefig("./Tests/ACF.png")
 
-# pollution_data = dataset[f"{pollutant} concentration"].squeeze('band')
+# # build ARIMA model and plot
+# model = ARIMA(mean_data_t_series_df[f"{pollutant} concentration"], order=(0,0,1))
+# model_fit = model.fit()
+# print(model_fit.summary())
+
+# residuals = pd.DataFrame(model_fit.resid)
+# fig, ax = plt.subplots(1,2)
+# residuals.plot(title="Residuals", ax=ax[0])
+# residuals.plot(kind='kde', title='Density', ax=ax[1])
+# plt.savefig("./Tests/Residuals.png")
+
+# mean_data_t_series_df["ARIMA"] = model_fit.predict()
 # fig = plt.figure()
+# sns.lineplot(data=mean_data_t_series_df)
+# plt.xticks(rotation=90)
+# plt.savefig(f"./Visualizations/{urban_area}{pollutant}ARIMAmean_time_series.png")
 
-# def animate(frame):
-#     fig.clear()
-#     ax = plt.subplot()
-#     current_data = pollution_data[frame, :, :]
-#     img = ax.imshow(current_data, origin='lower', vmin=0, vmax=bounds_dict[pollutant])
-#     current_time = times[frame].values
-#     ax.set_title(f'{pollutant} ({units_dict[pollutant]}) - {current_time}')
-#     plt.colorbar(img, ax=ax)
+bounds_dict = {"NO2": 53, "O3": 70, "PM": 35}
 
-# num_frames = pollution_data.shape[0]
-# anim = animation.FuncAnimation(fig, animate, frames=num_frames, interval=250)
-# writer = animation.FFMpegWriter(fps=8)
-# anim.save(f"./Visualizations/{urban_area}{pollutant}.mp4", writer=writer)
+pollution_data = dataset[f"{pollutant} concentration"].squeeze('band')
+fig = plt.figure()
+
+def animate(frame):
+    fig.clear()
+    ax = plt.subplot()
+    current_data = pollution_data[frame, :, :]
+    img = ax.imshow(current_data, origin='lower', vmin=0, vmax=bounds_dict[pollutant])
+    current_time = times[frame].values
+    ax.set_title(f'{pollutant} ({units_dict[pollutant]}) - {current_time}')
+    plt.colorbar(img, ax=ax)
+
+num_frames = pollution_data.shape[0]
+anim = animation.FuncAnimation(fig, animate, frames=num_frames, interval=250)
+writer = animation.FFMpegWriter(fps=8)
+anim.save(f"./Visualizations/{urban_area}{pollutant}.mp4", writer=writer)
